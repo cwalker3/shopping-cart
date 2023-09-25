39b25d56ebec44ca24fe2b5a495eef23fbe6c993
Cory Walker <cory.walker.dev@gmail.com>
2023-09-25 12:36:27 -0700

    add nav content

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,3 +1,14 @@
+import styles from "./Nav.module.scss";
+import { Link } from "react-router-dom";
+
 export default function Nav() {
-  return "nav";
+  return (
+    <div className={styles.nav}>
+      <div className={styles.navLinks}>
+        <Link to="/">Home</Link>
+        <Link to="shop">Shop</Link>
+      </div>
+      <div className={styles.cart}>Cart</div>
+    </div>
+  );
 }
