fcb03c826bee4bcfbbfcbefd1efa0f8bcd503c22
Cory Walker <cory.walker.dev@gmail.com>
2023-09-25 11:58:23 -0700

    create router

---
+++ src/Router.jsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Home from "./components/Home/Home";
+import Shop from "./components/Shop/Shop";
+import ErrorPage from "./components/ErrorPage/ErrorPage";
+
+const Router = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "/shop",
+          element: <Shop />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+};
+
+export default Router;
